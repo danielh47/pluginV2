4ba1973167038834233c6af43d7300046f695961
Daniel <harbuz.daniel@gmail.com>
2012-10-16 00:39:55 +0200

    Add My Plugin Options page in admin panel
    1. Page is added in Options and named My Plugin

--- myplugin.php
+++ myplugin.php
@@ -39,3 +39,19 @@ function printHelloWord()
 add_action('login_footer', 'changeCSSInLoginPanel');
 add_action('get_footer', 'printHelloWord');
 ?>
+<?php
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+?>
