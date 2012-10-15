ee45836c80f69561b8f53ebd433c9b954a0b8d9f
Daniel <harbuz.daniel@gmail.com>
2012-10-15 20:52:39 +0200

    Change the look of the login panel 2 (change logo)
    1. Add images/logo.png file
    2. Change logo in login menu (wp-login.php)

--- myplugin.php
+++ myplugin.php
@@ -4,8 +4,12 @@ Plugin Name: myplugin
  */
 function changeCSSInLoginPanel()
 {
-    
     echo '<link rel="Stylesheet" type="text/css" href="wp-content/plugins/myplugin/CSS/loginPanel.css" />';
+    echo '<style type="text/css">
+        h1 a {
+            background: url(wp-content/plugins/myplugin/images/logo.png) no-repeat top center !important; 
+        }
+        </style>';
 }
 
 
