4313b14a6baedcdea928a1443775822c72c5e467
Daniel <harbuz.daniel@gmail.com>
2012-10-15 20:38:58 +0200

    Change the look of the login panel
    1. Add CSS/loginPanel.css file
    2. Add function changeCSSInLoginPanel in myplugin.php

--- myplugin.php
+++ myplugin.php
@@ -2,4 +2,12 @@
 /*
 Plugin Name: myplugin
  */
+function changeCSSInLoginPanel()
+{
+    
+    echo '<link rel="Stylesheet" type="text/css" href="wp-content/plugins/myplugin/CSS/loginPanel.css" />';
+}
+
+
+add_action('login_footer', 'changeCSSInLoginPanel');
 ?>
