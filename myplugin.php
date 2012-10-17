e521e49d3737d91131737157656124867c01d0a5
Daniel <harbuz.daniel@gmail.com>
2012-10-17 22:31:13 +0200

    Add logic in options pannel
    1. Aaa form in options panel
    2. add handling form who decide if changes background color in home page
    3. Add function changeBackgroundColor()
    4. Moved javascript from printHelloWord() to changeBackgroundCollor()

--- myplugin.php
+++ myplugin.php
@@ -2,9 +2,28 @@
 /*
 Plugin Name: myplugin
  */
-?>
+session_start();
+if(!isset($_SESSION['changeMyBackground']))
+{
+    $_SESSION['changeMyBackground'] = true;
+}
+if(isset($_POST['changeMyBackground']))
+{
+    if($_POST['changeMyBackground'] == "1")
+    {
+        echo $_SESSION['changeMyBackground'] = 1;
+    }
+    else 
+    {
+        echo $_SESSION['changeMyBackground'] = 0;
+    }
+}
 
-<?php
+add_action('login_footer', 'changeCSSInLoginPanel');
+add_action('get_footer', 'printHelloWord');
+add_action( 'admin_menu', 'my_plugin_menu' );
+if($_SESSION['changeMyBackground'] == true)
+    add_action('get_footer', 'changeBackgroundColor');
 
 function changeCSSInLoginPanel()
 {
@@ -18,40 +37,50 @@ function changeCSSInLoginPanel()
 
 function printHelloWord()
 {
-    ?>
-
-<script type="text/javascript">
-    window.addEventListener('load', function () {
-    var backgroundColors = ['blue', 'yellow', 'green'];    
-    var i = 0;
-    setInterval(function () {
-        i++;
-        i%=backgroundColors.length;
-        
-        document.body.style.backgroundColor = backgroundColors[i];
-    }, 2000);
-}, false);
-</script>
-<?php
     echo "Hello Word";
 }
-
-add_action('login_footer', 'changeCSSInLoginPanel');
-add_action('get_footer', 'printHelloWord');
-?>
-<?php
-add_action( 'admin_menu', 'my_plugin_menu' );
 
 function my_plugin_menu() {
 	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
 }
 
 function my_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	if(!current_user_can('manage_options'))
+        {
+            wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
+        if($_SESSION['changeMyBackground'] == true)
+        { ?>
+            <form action="index.php" method=post>
+                <input id="changeMyBackground1" type="radio" name="changeMyBackground" value="1" checked="checked" />Zmieniaj kolor tła co 2 sekundy<br>
+                <input id="changeMyBackground0" type="radio" name="changeMyBackground" value="0" />Nie zmieniaj koloru tła<br>
+                <input type=submit name=edit value=Zmień />
+            </form>
+        <?php }
+        else
+        { ?>
+            <form action="index.php" method=post>
+                <input id="changeMyBackground1" type="radio" name="changeMyBackground" value="1" />Zmieniaj kolor tła co 2 sekundy<br>
+                <input id="changeMyBackground0" type="radio" name="changeMyBackground" value="0" checked="checked" />Nie zmieniaj koloru tła<br>
+                <input type=submit name=edit value=Zmień />
+            </form>
+        <?php }
+#changeMyBackground->
 }
-?>
+
+function changeBackgroundColor()
+{ ?>
+        <script type="text/javascript">
+            window.addEventListener('load', function () {
+            var backgroundColors = ['blue', 'yellow', 'green'];    
+            var i = 0;
+            setInterval(function () {
+                i++;
+                i%=backgroundColors.length;
+
+                document.body.style.backgroundColor = backgroundColors[i];
+            }, 2000);
+        }, false);
+        </script>
+    <?php
+} ?>
